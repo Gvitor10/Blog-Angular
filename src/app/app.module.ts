db8ec65d6eed5ee7c4fad4e0691ac61e40e52efb
Gvitor10 <gvitor7062@gmail.com>
2023-10-20 08:49:19 -0300

    Stylized page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeTitleComponent } from './components/home-title/home-title.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OtherCardComponent } from './components/other-card/other-card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeTitleComponent,
+    BigCardComponent,
+    SmallCardComponent,
+    HomeComponent,
+    OtherCardComponent,
   ],
   imports: [
     BrowserModule
